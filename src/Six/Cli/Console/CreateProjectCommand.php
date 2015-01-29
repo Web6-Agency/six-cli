279384bb8f650b0909684197fcdf0aa61ee6ea18
Favre Anael <anael.favre@gmail.com>
2015-01-29 22:08:59 +0100

    Adding create project command

---
+++ src/Six/Cli/Console/CreateProjectCommand.php
@@ -0,0 +1,56 @@
+<?php namespace Six\Cli\Console;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+
+class CreateProjectCommand extends Command {
+
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'project:create';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create a new 6admin project based on private repo';
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function fire()
+    {
+        
+    }
+
+    /**
+     * Get the console command arguments.
+     *
+     * @return array
+     */
+    protected function getArguments()
+    {
+        return [
+            
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            
+        ];
+    }
+
+
+}
